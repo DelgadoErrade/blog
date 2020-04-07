714c3e1a787e67b41612f0447917353e1d3861e2
Jose Delgado <delgadoerrade@gmail.com>
2020-04-07 12:57:55 -0400

    Se establecieron las relaciones entre las tablsa cambiando los archivos category, post, tab y user

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'
+    ];
+    public function user(){
+        return $this->belongsToMany(User::class);
+    }
+
+    public function Category(){
+        return $this->belongsToMany(Category::class);
+    }
+
     public function tags(){
         return $this->belongsToMany(Tag::class);
     }
